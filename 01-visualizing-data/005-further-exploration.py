5e1d15e4200c0efac5cd8714f09e72cd1cef4cec
Assad zakir <assadzakirhussain@gmail.com>
2017-10-22 23:52:48 +0500

    -Visualizing data with matplotlib

---
+++ 01-visualizing-data/005-further-exploration.py
@@ -0,0 +1,14 @@
+# For Further Exploration
+
+# • seaborn is built on top of matplotlib and allows you to easily produce prettier
+# (and more complex) visualizations.
+
+# • D3.js is a JavaScript library for producing sophisticated interactive visualizations
+# for the web. Although it is not in Python, it is both trendy and widely used, and it
+# is well worth your while to be familiar with it.
+
+# • Bokeh is a newer library that brings D3-style visualizations into Python.
+
+# • ggplot is a Python port of the popular R library ggplot2 , which is widely used for
+# creating “publication quality” charts and graphics. It’s probably most interesting
+# if you’re already an avid ggplot2 user, and possibly a little opaque if you’re not.
